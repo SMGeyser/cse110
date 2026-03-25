5c2cb4cd520eacc120f39ee2cd14623054ddc6c2
SMGeyser <samgeyser@gmail.com>
2026-03-25 13:05:31 -0600

    Update week12_team.py

--- week12_team.py
+++ week12_team.py
@@ -3,12 +3,11 @@ chapters = []
 book = []
 
 with open("books_and_chapters.txt") as line:
-    next(line)
     for data in line:
         data = data.split(":")
         scripture.append(data[0])
-        chapters.append(data[1])
-        book.append(int(data[2]))
+        chapters.append(int(data[1]))
+        book.append((data[2]))
 
 for i in range(len(scripture)):
     print(f"Scripture: {scripture[i]}, Book: {book[i]}, Chapters: {chapters[i]}")
