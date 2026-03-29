c25f86c563819eb4e71ac23a16dcb0e5aa3bd648
SMGeyser <samgeyser@gmail.com>
2026-03-28 22:30:48 -0600

    finished home page css

--- week11_prove.py
+++ week11_prove.py
@@ -13,32 +13,6 @@ with open("life-expectancy.csv") as line:
         year.append(int(data[2]))
         age.append(float(data[3]))
 
-select_year = int(input("Select a year you want to learn about: "))
-in_year = []
-in_country = []
-in_age = []
-for i in range(len(year)):
-    if year[i] == select_year:
-        in_year.append(year[i])
-        in_country.append(country[i])
-        in_age.append(age[i])
-print(f"The average life expectancy across all countries in {select_year} was {(sum(in_age)/len(in_age)):.3f}")
-in_most_age = 0
-for i in range(len(in_age)):
-    if in_age[i] > in_most_age:
-        in_most_age = in_age[i]
-        in_most_country = in_country[i]
-        in_most_year = in_year[i]
-in_least_age = in_most_age
-for i in range(len(in_age)):
-    if in_age[i] < in_least_age:
-        in_least_age = in_age[i]
-        in_least_country = in_country[i]
-        in_least_year = in_year[i]
-print(f"The maximum life expectancy in {in_most_year} comes from {in_most_country}. It is {in_most_age}.")
-print(f"The minimum life expectancy in {in_least_year} comes from {in_least_country}. It is {in_least_age}.")
-
-
 most_age = 0
 for i in range(len(age)):
     if age[i] > most_age:
@@ -57,3 +31,34 @@ for i in range(len(age)):
 
 print(f"\nThe maximum overall life expectancy comes from {most_country} in {most_year}. It is {most_age}.")
 print(f"The minimum overall life expectancy comes from {least_country} in {least_year}. It is {least_age}.")
+
+done = False
+while not done:
+    select_year = int(input("Select a year you want to learn about: "))
+    in_year = []
+    in_country = []
+    in_age = []
+    for i in range(len(year)):
+        if year[i] == select_year:
+            in_year.append(year[i])
+            in_country.append(country[i])
+            in_age.append(age[i])
+    print(f"The average life expectancy across all countries in {select_year} was {(sum(in_age)/len(in_age)):.3f}")
+    in_most_age = 0
+    for i in range(len(in_age)):
+        if in_age[i] > in_most_age:
+            in_most_age = in_age[i]
+            in_most_country = in_country[i]
+            in_most_year = in_year[i]
+    in_least_age = in_most_age
+    for i in range(len(in_age)):
+        if in_age[i] < in_least_age:
+            in_least_age = in_age[i]
+            in_least_country = in_country[i]
+            in_least_year = in_year[i]
+    print(f"The maximum life expectancy in {in_most_year} comes from {in_most_country}. It is {in_most_age}.")
+    print(f"The minimum life expectancy in {in_least_year} comes from {in_least_country}. It is {in_least_age}.")
+    finish = input("Would you like to learn about another year? (Y/N) ")
+    if finish.lower().strip() == "n":
+        done = True
+print("Ok, bye. Have a good one.")
--- week12_team.py
+++ week12_team.py
@@ -4,10 +4,88 @@ book = []
 
 with open("books_and_chapters.txt") as line:
     for data in line:
-        data = data.split(":")
-        scripture.append(data[0])
+        data = data.strip().split(":")
+        scripture.append(data[2])
         chapters.append(int(data[1]))
-        book.append((data[2]))
+        book.append((data[0]))
 
 for i in range(len(scripture)):
     print(f"Scripture: {scripture[i]}, Book: {book[i]}, Chapters: {chapters[i]}")
+
+high = chapters[0]
+for i in range(len(chapters)):
+    if high < chapters[i]:
+        high = chapters[i]
+        high_book = book[i]
+
+print(f"The highest amount of chapters is from the book of {high_book} with {high} chapters.")
+
+choice = input("Which book would you like to learn more about? (BoM, OT, NT, D&C, PoGP) ")
+
+if choice.strip().lower() == "bom":
+    bom = []
+    bom_chap = []
+    high_bom = 0
+    for i in range(len(scripture)):
+        if scripture[i].strip().lower() == "book of mormon":
+            bom.append(book[i])
+            bom_chap.append(chapters[i])
+    for i in range(len(bom)):
+        if high_bom < bom_chap[i]:
+            high_bom = bom_chap[i]
+            high_bom_book = bom[i]
+    print(f"The book with the most chapters in the Book of Mormon is {high_bom_book} with {high_bom} chapters")
+if choice.strip().lower() == "ot":
+    ot = []
+    ot_chap = []
+    high_ot = 0
+    for i in range(len(scripture)):
+        if scripture[i].strip().lower() == "old testament":
+            ot.append(book[i])
+            ot_chap.append(chapters[i])
+    for i in range(len(ot)):
+        if high_ot < ot_chap[i]:
+            high_ot = ot_chap[i]
+            high_ot_book = ot[i]
+    print(f"The book with the most chapters in the Old Testament is {high_ot_book} with {high_ot} chapters")
+if choice.strip().lower() == "nt":
+    nt = []
+    nt_chap = []
+    high_nt = 0
+    for i in range(len(scripture)):
+        if scripture[i].strip().lower() == "new testament":
+            nt.append(book[i])
+            nt_chap.append(chapters[i])
+    for i in range(len(nt)):
+        if high_nt < nt_chap[i]:
+            high_nt = nt_chap[i]
+            high_nt_book = nt[i]
+    print(f"The book with the most chapters in the New Testament is {high_nt_book} with {high_nt} chapters")
+if choice.strip().lower() == "d&c":
+    dc = []
+    dc_chap = []
+    high_dc = 0
+    for i in range(len(scripture)):
+        if scripture[i].strip().lower() == "doctrine and covenants":
+            dc.append(book[i])
+            dc_chap.append(chapters[i])
+    for i in range(len(dc)):
+        if high_dc < dc_chap[i]:
+            high_dc = dc_chap[i]
+            high_dc_book = dc[i]
+    print(f"The book with the most chapters in the Doctrine and Covenants is {high_dc_book} with {high_dc} chapters")
+if choice.strip().lower() == "pogp":
+    pogp = []
+    pogp_chap = []
+    high_pogp = 0
+    for i in range(len(scripture)):
+        if scripture[i].strip().lower() == "pearl of great price":
+            pogp.append(book[i])
+            pogp_chap.append(chapters[i])
+    for i in range(len(pogp)):
+        if high_pogp < pogp_chap[i]:
+            high_pogp = pogp_chap[i]
+            high_pogp_book = pogp[i]
+    print(f"The book with the most chapters in the Pearl of Great Price is {high_pogp_book} with {high_pogp} chapters")
+
+        
